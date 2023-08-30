2fae0023c926c17a6fe79f7d2197e0a45ce989b5
VIETDOM\luatd <jeffrey.tdl21@gmail.com>
2023-08-30 10:21:19 +0700

    Revert "fix sample 09"
    This reverts commit e1a2e8443fafd4ccc61d0317b1b41b319d9512ac.

--- tekla_training/Sample_09_abstract/Sample_09_abstract/Shape.cs
+++ tekla_training/Sample_09_abstract/Sample_09_abstract/Shape.cs
@@ -6,34 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sample_09_abstract
 {
-    public abstract class Shape
+    internal class Shape
     {
-        #region Properties
-        private string _name;
-        private Point2d _insertPoint;
-
-        public string Name { get => _name; set => _name = value; }
-        public Point2d InsertPoint { get => _insertPoint; set => _insertPoint = value; }
-        #endregion
-
-        #region Methods
-
-        // Cách khai báo phương thức trừu tượng
-        public abstract double Area();
-
-        public abstract double Perimeter();
-
-
-        // Các khai báo phương thức ảo
-
-        public virtual void Move(double deltaX, double deltaY)
-        {
-            this._insertPoint.X += deltaX;
-            this._insertPoint.Y += deltaY;
-        }
-
-
-        #endregion
-
     }
 }
